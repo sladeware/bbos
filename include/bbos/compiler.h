246658b35ace3c862d9243da30db8137e1dd6fce
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-07-11 10:17:52 +0000

    GNUC compiler traps.

--- include/bbos/compiler.h
+++ include/bbos/compiler.h
@@ -5,6 +5,11 @@
 #ifndef __BBOS_COMPILER_H
 #define __BBOS_COMPILER_H
 
+/* Trap pasters of __FUNCTION__ at compiler-time */
+#if __GNUC__ > 2 || __GNUC_MINOR__ >= 95
+#define __FUNCTION__ (__func__)
+#endif
+
 /* Catalina compiler */
 #ifdef __CATALINA__
 #include <bbos/compiler/catalina.h>
