8aeb149990fac0b2405fc97e6f5b9eaed31d6f80
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-07-29 22:15:24 +0000

    Add P8X32A-Q44 platform support.

---
+++ include/bbos/hardware/platform/P8X32A-Q44/init.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2010 Slade Maurer, Alexander Sviridenko
+ */
+
+#ifndef __PLATFORM_P8X32A_Q44_INIT_H
+#define __PLATFORM_P8X32A_Q44_INIT_H
+
+void platform_init();
+
+#endif /* __PLATFORM_P8X32A_Q44_INIT_H */
+
+
---
+++ src/hardware/platform/P8X32A-Q44/init.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2010 Slade Maurer, Alexander Sviridenko
+ */
+
+void
+platform_init()
+{
+}
+
+
+
