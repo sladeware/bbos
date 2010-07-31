73b1455b8cf90f7001d470818182ed4955606b4f
Slade Maurer <slade@computer.org>
2010-07-31 07:41:58 +0000

    I'll run this script in cron and it will detect changes in the repo. Right now it will email active developers so everyone knows the repo changed. In the near future, this will be an entry point for our testing system to verify each revision continues to work properly on our hardware.

---
+++ scripts/detect_changes_in_svn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Detect changes in the svn repository and kick off
+# processing as required
+#
+
+cd /home/slade/bbos/src/bbos
+
+TMP=/tmp/.bbos_head
+BBOS_HEAD=$(svn log -r HEAD | awk '/^r[0-9]+/ {print $1}')
+
+SUBJECT="New BBOS Revision"
+TO_ADDR="slade@computer.org mail.d2rk@gmail.com"
+
+if [ -e "$TMP" ];
+then
+    LAST_BBOS_HEAD=$(cat "$TMP")
+    if [ "$BBOS_HEAD" != "$LAST_BBOS_HEAD" ];
+    then
+        echo "Emailing $TO_ADDR about $BBOS_HEAD"
+	svn log -r HEAD | nail -s "$SUBJECT $BBOS_HEAD" "$TO_ADDR";
+    fi;
+fi;
+
+echo "$BBOS_HEAD" > "$TMP"
