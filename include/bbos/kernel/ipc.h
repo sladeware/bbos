9bb6f19f2e402b5fe1f65488655ac41b1bc16b3f
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-09-22 22:11:56 +0000

    Start IPC support.

---
+++ include/bbos/kernel/ipc.h
@@ -0,0 +1,10 @@
+/*
+ * Inter-Process Communication support.
+ *
+ * Copyright (c) 2010 Slade Maurer, Alexander Sviridenko
+ */
+
+#ifndef __BBOS_IPC_H
+#define __BBOS_IPC_H
+
+#endif /* __BBOS_IPC_H */
