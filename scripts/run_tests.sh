dc7bcec872d2fe21a9bf63854d5b1c5fdf8e1dbd
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-10-28 20:10:15 +0000

    Restructure builder, part 2.

---
+++ scripts/run_tests.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+#
+# Run all the test required before commiting bbos builder code
+#
+# Copyright (c) 2010 Slade Maurer, Alexander Sviridenko
+#
+
+seperator() {
+    echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+}
+
+# Output banner
+clear
+seperator
+echo "BBOS Test Runner!"
+seperator
+
+# You need to have pychecker installed to do a pycheck
+which pychecker > /dev/null
+if [ "$?" -eq "0" ]; then
+    RESULT=`find . -name \*.py | xargs pychecker 2> /dev/null`
+    echo
+    echo "PYCHECKER RESULTS. PLEASE REVIEW AND CLEANUP WARNINGS."
+    seperator
+    echo
+    echo $RESULT
+    echo
+else
+    echo "YOU MUST INSTALL PYCHECKER TO RUN THE PYCHECKER TEST"
+fi
+
+# Run the bbos builder library regressions
+which python > /dev/null
+if [ "$?" -eq "0" ]; then
+    echo
+    echo "REGRESSION TEST RESULTS. PLEASE FIX FAILURES."
+    seperator
+    echo
+    python lib/regression.py
+    echo
+else
+    echo "YOU MUST INSTALL PYTHON TO RUN PYTHON PREGRESSION TESTS"
+fi
+seperator
