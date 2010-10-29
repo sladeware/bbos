7c20fab758348df089fde7d42c7d14ac5224a7aa
Slade Maurer <slade@computer.org>
2010-10-29 15:44:31 +0000

    Made the run tests script work when started out of any directory to support extended detect changes capabilities.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,6 +5,9 @@
 # Copyright (c) 2010 Slade Maurer, Alexander Sviridenko
 #
 
+BBOS_ROOT=`dirname $0`/..
+PYTHONPATH=$PYTHONPATH:$BBOS_ROOT
+
 seperator() {
     echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 }
@@ -15,19 +18,6 @@ seperator
 echo "BBOS Test Runner!"
 seperator
 
-# You need to have pychecker installed to do a pycheck
-which pychecker > /dev/null
-if [ "$?" -eq "0" ]; then
-    echo
-    echo "PYCHECKER RESULTS. PLEASE REVIEW AND CLEANUP WARNINGS."
-    seperator
-    echo
-    find ../bbos -name \*.py | xargs pychecker 2> /dev/null
-    echo
-else
-    echo "YOU MUST INSTALL PYCHECKER TO RUN THE PYCHECKER TEST"
-fi
-
 # Run the bbos builder library regressions
 which python > /dev/null
 if [ "$?" -eq "0" ]; then
@@ -35,9 +25,23 @@ if [ "$?" -eq "0" ]; then
     echo "REGRESSION TEST RESULTS. PLEASE FIX FAILURES."
     seperator
     echo
-    python ../bbos/builder/regression.py
+    python $BBOS_ROOT/bbos/builder/regression.py
     echo
 else
     echo "YOU MUST INSTALL PYTHON TO RUN PYTHON PREGRESSION TESTS"
+fi
+seperator
+
+# You need to have pychecker installed to do a pycheck
+which pychecker > /dev/null
+if [ "$?" -eq "0" ]; then
+    echo
+    echo "PYCHECKER RESULTS. PLEASE REVIEW AND CLEANUP WARNINGS."
+    seperator
+    echo
+    find $BBOS_ROOT -name \*.py | xargs pychecker
+    echo
+else
+    echo "YOU MUST INSTALL PYCHECKER TO RUN THE PYCHECKER TEST"
 fi
 seperator
