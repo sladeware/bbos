b947bee12da00367761f06f7232245c21f00a4c0
Slade Maurer <slade@computer.org>
2010-10-30 00:32:19 +0000

    Updated the commit tests so that hello world is built and ran during the tests using bbos_builder.py. This way we can make sure it all works just by looking at the email on each commit to see if the generate, build and run work OK. Also, I tidied up the pychecker output so it is easier to read.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,6 +8,9 @@
 BBOS_ROOT=`dirname $0`/..
 export PYTHONPATH=$BBOS_ROOT:$PYTHONPATH
 
+DEMO=$BBOS_ROOT/demos/hello_world/demo
+BBOS_H=$BBOS_ROOT/demos/hello_world/bbos.h
+
 seperator() {
     echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 }
@@ -18,10 +21,11 @@ echo "BBOS Test Runner!"
 seperator
 
 # Run the bbos builder library regressions
+CNT=1
 which python > /dev/null
 if [ "$?" -eq "0" ]; then
     echo
-    echo "REGRESSION TEST RESULTS. PLEASE FIX FAILURES."
+    echo "[$CNT] REGRESSION TEST RESULTS. PLEASE FIX FAILURES."
     seperator
     echo
     python $BBOS_ROOT/bbos/builder/regression.py
@@ -31,14 +35,59 @@ else
 fi
 seperator
 
+# Build hello world (we know python is installed)
+CNT=2
+echo
+echo "[$CNT] GENERATING AND BUILDING HELLO WORLD. PLEASE FIX FAILURES"
+seperator
+echo
+date
+echo
+python $BBOS_ROOT/scripts/bbos_builder.py -a $BBOS_ROOT/demos/hello_world/bbos.py
+echo
+seperator
+
+# List the results so we know it worked
+CNT=3
+echo
+echo "[$CNT] LISTING THE RESULTS SO YOU KNOW THE BBOS BUILDER WORKED"
+seperator
+ls -l $BBOS_H
+ls -l $DEMO
+echo
+seperator
+
+# Show the hello world header file
+CNT=4
+echo
+echo "[$CNT] SHOWING YOU THE BBOS HEADER THAT WAS GENERATED IN CASE YOU THINK IT IS WRONG"
+seperator
+cat $BBOS_H
+echo
+seperator
+
+# Run hello world
+CNT=5
+echo
+echo "[$CNT] RUNNING HELLO WORLD. PLEASE FIX FAILURES."
+seperator
+echo
+chmod +x $DEMO
+$DEMO | head &
+sleep 1
+kill %1
+echo
+seperator
+
 # You need to have pychecker installed to do a pycheck
+CNT=6
 which pychecker > /dev/null
 if [ "$?" -eq "0" ]; then
     echo
-    echo "PYCHECKER RESULTS. PLEASE REVIEW AND CLEANUP WARNINGS."
+    echo "[$CNT] PYCHECKER RESULTS. PLEASE REVIEW AND CLEANUP WARNINGS."
     seperator
     echo
-    find $BBOS_ROOT -name \*.py | xargs pychecker
+    find $BBOS_ROOT -name \*.py | xargs pychecker 2> /dev/null
     echo
 else
     echo "YOU MUST INSTALL PYCHECKER TO RUN THE PYCHECKER TEST"
