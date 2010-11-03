04cba619e4a246bd41ca45dba3daf0a1c1b2c7c6
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-11-03 20:15:01 +0000

    Fixed bug with write function. Added a few more asserts.

--- bbos/kernel/process/port.h
+++ bbos/kernel/process/port.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) ???? Slade Maurer, Alexander Sviridenko
+ * Copyright (c) 2011 Slade Maurer, Alexander Sviridenko
  *
  */
 #ifndef __BBOS_PORT_H
@@ -10,10 +10,8 @@
 #define bbos_port_get_input(id)   bbos_port_table[id].in
 #define bbos_port_get_output(id)  bbos_port_table[id].out
 
-typedef int16_t bbos_port_id_t;
-
 struct bbos_port {
-  uint8_t *buffer;
+  void *buffer;
   uint16_t in;
   uint16_t out;
   uint16_t size;
@@ -21,10 +19,15 @@ struct bbos_port {
 
 extern struct bbos_port bbos_port_table[BBOS_NUMBER_OF_PORTS];
 
-void bbos_port_init(bbos_port_id_t id, uint8_t *buffer, uint16_t size);
-uint16_t bbos_port_read(bbos_port_id_t id, uint8_t *buffer, uint16_t n);
-bbos_uint16_t bbos_port_write(bbos_port_id_t id, uint8_t *buffer, uint16_t n);
+void bbos_port_init(bbos_port_id_t id, void *buffer, uint16_t size);
+
+uint16_t bbos_port_read(bbos_port_id_t id, void *buffer, uint16_t n);
+
+uint16_t bbos_port_write(bbos_port_id_t id, void *buffer, uint16_t n);
+
 void bbos_port_flush(bbos_port_id_t id);
+
+int8_t bbos_port_empty(bbos_port_id_t id);
 
 #else
 #define BBOS_NUMBER_OF_PORTS 0
