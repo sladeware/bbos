6c61205fba370b10693baf86fd5edcab813b6ec3
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-11-03 20:21:55 +0000

    First version of GPIO test.

---
+++ demos/gpio/bbos.h
@@ -0,0 +1,23 @@
+//
+
+#ifndef __BBOS_H
+#define __BBOS_H
+
+#define HAVE_INTTYPES_H
+#define HAVE_STDINT_H
+#define HAVE_STDDEF_H
+
+#define P8X32A_GPIO_ID 0
+
+#define BBOS_NUMBER_OF_APPLICATION_THREADS 1
+
+#define TEST_PORT_ID 0
+#define P8X32A_GPIO_PORT_ID 1
+
+/* The number of ports in this process */
+#define BBOS_NUMBER_OF_PORTS 2
+
+#include <bbos/kernel.h>
+
+#endif
+
