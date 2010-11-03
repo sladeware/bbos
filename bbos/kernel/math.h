74d144d1eecee8f749049400a4d3b13221855469
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-11-03 20:11:00 +0000

    Added math support. Plan to add math32 and math64 support.

---
+++ bbos/kernel/math.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2011 Slade Maurer, Alexander Sviridenko
+ */
+
+#ifndef __BBOS_MATH_H
+#define __BBOS_MATH_H
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+
+#endif /* __BBOS_MATH_H */
+
+
