e311314a00822c884eec23b0e1fc166baff9ce7e
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2010-11-04 23:20:59 +0000

    Start working on generic config. This config will be used by the source files and will include all of the BBOS components. Thus bbos.h file will be much safe and even can be *empty*.

---
+++ bbos/config.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2011 Slade Maurer, Alexander Sviridenko
+ */
+
+#ifndef __BBOS_CONFIG_H
+#define __BBOS_CONFIG_H
+
+#include <bbos.h>
+
+#include <bbos/compiler.h>
+#include <bbos/hardware.h>
+#include <bbos/kernel.h>
+
+#endif
+
