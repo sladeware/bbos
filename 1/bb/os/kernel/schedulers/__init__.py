1126c0987b75ce124c9d318e7d861e35a2745f8e
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-06-21 13:29:17 +0000

    FCFS scheduler

--- 1/bb/os/kernel/schedulers/__init__.py
+++ 1/bb/os/kernel/schedulers/__init__.py
@@ -1,3 +1,6 @@
 
+__copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
+
 from bb.os.kernel.schedulers.staticscheduler import StaticScheduler
 from bb.os.kernel.schedulers.dynamicscheduler import DynamicScheduler
+from bb.os.kernel.schedulers.fcfsscheduler import FCFSScheduler
