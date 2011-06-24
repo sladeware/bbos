16e4f28a541db46c5ba3dfa67723c1ba63d58639
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-06-24 17:06:37 +0000

    Improving blinker

--- 1/tests/blinker/model.py
+++ 1/tests/blinker/model.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+__copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
 
 import time
 import sys
@@ -6,15 +9,13 @@ from bb.os.kernel import Kernel, Thread
 from bb.os.kernel.schedulers import StaticScheduler
 
 LED=1
+TIMEOUT=3
 
 def blink_runner():
     print "Blink LED#%d!" % LED
-    time.sleep(3) # sleep for 3 seconds
+    time.sleep(TIMEOUT) # in seconds
 
 blinker = Kernel()
 blinker.set_scheduler(StaticScheduler())
 blinker.add_thread("BLINK", blink_runner)
-
-
-
 
