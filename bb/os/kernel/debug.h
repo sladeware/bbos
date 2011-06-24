bf27298f4aa351f21f6ed7f4a82ba3051dbd5221
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-06-24 17:13:34 +0000

    Temporary C interface

---
+++ bb/os/kernel/debug.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2011 Slade Maurer, Alexander Sviridenko
+ */
+
+#ifndef __BBOS_DEBUG_H
+#define __BBOS_DEBUG_H
+
+/**
+ * @file debug.h
+ * @brief Debug
+ */
+
+#include <assert.h>
+
+#ifndef BBOS_DEBUG
+/**
+ * The BBOS debug macro equals to the ANSI standard NDEBUG macro.
+ * Learn more http://www.embedded.com/story/OEG20010311S0021
+ */
+#define BBOS_DEBUG NDEBUG
+#endif
+
+#endif
+
