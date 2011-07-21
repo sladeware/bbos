4e0fccbf9d737f7f1a3c2649a05a07d3e17d0257
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-21 14:14:02 +0000

    Blinker test with some renames

--- 1/tests/blinker/blinker.py
+++ 1/tests/blinker/blinker.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
 
-"""Design blinker's process"""
+"""Design blinker's application"""
 
 __copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
 
-import time
-
-from bb import os
 from bb import app
 
-LED=1
-TIMEOUT=3
+blinker = app.new_application()
+process = blinker.add_process('blinker')
 
-def blink_runner():
-    print "Blink LED#%d!" % LED
-    time.sleep(TIMEOUT) # in seconds
-
-kernel = os.Kernel()
-kernel.set_scheduler(os.StaticScheduler())
-kernel.add_thread("BLINK", blink_runner)
+from bb.os.hardware.boards import PropellerDemoBoard
+board = PropellerDemoBoard([process])
 
 if app.get_mode() is app.SIMULATION_MODE:
-	kernel.start()	
+	blinker.run()
 
