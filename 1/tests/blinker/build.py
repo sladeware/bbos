599f221603a4d2114f520764d661e959df732fbe
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-21 14:12:58 +0000

    Blinker test

--- 1/tests/blinker/build.py
+++ 1/tests/blinker/build.py
@@ -5,25 +5,22 @@ __copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
 import sys
 import module
 
-#from model import blinker
-import bb.app as app
-from bb.os.hardware.boards import PropellerDemoBoard
+from bb import app
 from bb.builder.projects import CatalinaProject
 
-app.MODE = "BUILING"
+from application import blinker
 
-def build(process):
-    board = PropellerDemoBoard([process.kernel])
-    project = CatalinaProject("Blinker", [process.kernel])
-    for source_file in ('propeller_demo_board.c',):
-        project.add_source(module.get_file(__name__, source_file))
-    compiler = project.get_compiler()
-    compiler.add_include_dir(module.get_dir())
-    compiler.add_library('ci')
-    compiler.define_macro("LED", 18)
-    project.build(verbose=False, dry_run=False)
+project = CatalinaProject("Blinker", [blinker])
+for source_file in ('blinker.c',):
+	project.add_source(module.get_file(__name__, source_file))
 
-if not app.has_processes():
-    app.add_process("blinker")
-for process in app.load_processes():
-    build(process)
+# Setup compiler
+compiler = project.get_compiler()
+compiler.add_include_dir(module.get_dir())
+compiler.add_library('ci')
+compiler.define_macro("LED", 18)
+
+# Build application
+project.build(verbose=False, dry_run=False)
+
+
