fedb21c8f387c1f43ec3eeff1bf6b102d2eaae5b
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-22 08:36:45 +0000

    Start working on H48C accel driver

---
+++ 1/bb/os/hardware/drivers/accel/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Accelerometer drivers"""
+
+__copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
+
+from bb.os.hardware.drivers.accel.h48c import H48CDriver
+
+
