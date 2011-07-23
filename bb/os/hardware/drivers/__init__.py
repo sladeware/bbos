c039c60380801b51d2d7b1236f6ca43d7d227534
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-23 21:48:45 +0000

    Accel demo test

---
+++ bb/os/hardware/drivers/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+__copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
+
+# Import accelerometer drivers
+from bb.os.hardware.drivers.accel import *
+
--- bb/os/hardware/drivers/accel/h48c/__init__.py
+++ bb/os/hardware/drivers/accel/h48c/__init__.py
@@ -1,28 +1,36 @@
+#!/usr/bin/env python
 
 __copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
 
-import os
+import random
 
-from bbos.kernel.thread import Thread
-from bbos.kernel.module import Module
+from bb import os
+from bb.os.hardware import Driver
 
-class H48C(Module):
-    def __init__(self, kernel):
-        Module.__init__(self, "H48C Accelerometer Driver")
-        self.thread = Thread("H48C", "h48c")
-        # Introduce module to the kernel
-        kernel.add_thread( self.get_thread() )
-        kernel.add_messages( self.get_messages() )
+def h48c_open():
+    pass
+    
+def h48c_freefall():
+    return int(random.random() + 0.1)
 
-    def get_thread(self):
-        return self.thread
+class H48C_FREEFALL(os.Command):
+    '''Free fall detection'''
 
-    def get_messages(self):
-        return ["H48C_FREE_FALL", "H48C_GFORCE_AOX", 
-                "H48C_GFORCE_AOY", "H48C_GFORCE_AOZ"]
+class h48c(Driver):
+    name='H48C'
+    description='H48C Accelerometer Driver'
+    commands=[os.BBOS_DRIVER_OPEN, os.BBOS_DRIVER_CLOSE, H48C_FREEFALL]
 
-    def _attach(self, proj):
-        proj.compiler.add_include_dir(os.path.dirname(__file__))
-        proj.add_sources([os.path.join(os.path.dirname(__file__), "h48c.c"),
-                          os.path.join(os.path.dirname(__file__), "../spi/spi_stamp.c")])
+    @Driver.runner
+    def h48c_runner(self):
+        message = self.get_message()
+        if message:
+            if message.get_command() is os.BBOS_DRIVER_OPEN:
+                h48c_open()
+            elif message.get_command() is H48C_FREEFALL:
+                message.set_data(h48c_freefall())
+            sender = message.get_sender()
+            message.set_sender(self.name)
+            os.get_running_kernel().send_message(sender, message)
+
 
