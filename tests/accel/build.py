b6f51d4c731b0b2601ee99b5a91796374f733333
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-24 14:02:45 +0000

    Start working with build-system for accel

---
+++ tests/accel/build.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+__copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander Sviridenko"
+
+import sys
+import module
+
+from bb import app
+from bb.builder.projects import CatalinaProject
+
+from accel import accel
+
+project = CatalinaProject("Accel", [accel])
+for source_file in ('accel.c',):
+	project.add_source(module.get_file(__name__, source_file))
+
+# Setup compiler
+compiler = project.get_compiler()
+compiler.add_include_dir(module.get_dir())
+compiler.add_library('ci')
+
+# Loader
+from bb.builder.loaders import BSTLLoader
+project.set_loader(BSTLLoader())
+
+# Build application
+project.build(verbose=True, dry_run=False)
+
+#project.load()
+
