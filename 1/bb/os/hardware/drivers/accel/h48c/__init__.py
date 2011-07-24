6010f8391406a622d5f7e7c6a58a1983f7930d6f
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-24 20:55:34 +0000

    Build system for accel test

--- 1/bb/os/hardware/drivers/accel/h48c/__init__.py
+++ 1/bb/os/hardware/drivers/accel/h48c/__init__.py
@@ -13,13 +13,23 @@ def h48c_open():
 def h48c_freefall():
     return int(random.random() + 0.1)
 
+class H48C_GFORCE_AOX(os.Command):
+    pass
+
+class H48C_GFORCE_AOY(os.Command):
+    pass
+
+class H48C_GFORCE_AOZ(os.Command):
+    pass
+
 class H48C_FREEFALL(os.Command):
     """Free fall detection"""
 
 class h48c(Driver):
     name='H48C'
     description='H48C Accelerometer Driver'
-    commands=(os.BBOS_DRIVER_OPEN, os.BBOS_DRIVER_CLOSE, H48C_FREEFALL)
+    commands=(os.BBOS_DRIVER_OPEN, os.BBOS_DRIVER_CLOSE, H48C_FREEFALL, \
+              H48C_GFORCE_AOX, H48C_GFORCE_AOY, H48C_GFORCE_AOZ)
 
     @Driver.runner
     def h48c_runner(self):
@@ -33,4 +43,5 @@ class h48c(Driver):
             message.set_sender(self.name)
             os.get_running_kernel().send_message(sender, message)
 
+import bb.os.hardware.drivers.accel.h48c.setup
 
