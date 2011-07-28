ed7fcaa9d37707d18bb9bffeee0616f51cf02de5
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-28 10:02:45 +0000

    Start working on serial driver

--- bb/os/hardware/drivers/serial/__init__.py
+++ bb/os/hardware/drivers/serial/__init__.py
@@ -2,12 +2,36 @@
 
 __copyright__ = "Copyright (c) 2011 Slade Maurer, Alexander"
 
-from bbos.kernel.module import Module
+from bb.os import Command, BBOS_DRIVER_OPEN
+from bb.os.hardware import Driver
 
-class SerialCore(Module):
-    def __init__(self, kernel):
-        Module.__init__(self, "Standard serial driver")
-        
-    def get_commands(self):
-        return ["SERIAL_SEND_BYTE", "SERIAL_RECEIVE_BYTE"]
+class SERIAL_SEND_BYTE(Command):
+    pass
 
+class SERIAL_RECEIVE_BYTE(Command):
+    pass
+
+class serial(Driver):
+    name='Serial',
+    description='Standard serial driver',
+    commands=[BBOS_DRIVER_OPEN, SERIAL_SEND_BYTE, SERIAL_RECEIVE_BYTE]
+
+    def serial_open(self):
+        pass
+
+    def serial_send_byte(self):
+        pass
+
+    def serial_receive_byte(self):
+        pass
+
+    @Driver.runner
+    def serial_runner(self):
+        message = self.get_message()
+        if message:
+            if message.get_command() is BBOS_DRIVER_OPEN:
+                self.serial_open()
+            if message.get_command() is SERIAL_SEND_BYTE:
+                self.serial_send_byte()
+            elif message.get_command() is SERIAL_RECEIVE_BYTE:
+                self.serial_receive_byte()
