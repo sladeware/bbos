3d35c8981ccf5dbf3995b9a40e585a5d114ccc7b
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-07-28 12:23:01 +0000

    Fixed bug with the new c symbol feature for catalina compiler

--- tests/blinker/blinker.c
+++ tests/blinker/blinker.c
@@ -10,14 +10,14 @@
 
 #include <stdio.h>
 
-static int mask = GET_PIN(__CATALINA_LED);
-static int on_off = GET_PIN(__CATALINA_LED);
+static int mask = GET_PIN(LED);
+static int on_off = GET_PIN(LED);
 
 void
 blink_runner()
 {
-	GPIOA_SET_DIRECTION(__CATALINA_LED, on_off);
-  GPIOA_SET_VALUE(__CATALINA_LED, on_off);
+	GPIOA_SET_DIRECTION(LED, on_off);
+  GPIOA_SET_VALUE(LED, on_off);
   bbos_delay_sec(3);
   on_off ^= mask;
 }
