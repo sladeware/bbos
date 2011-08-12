1e21ceb60d20d1b72a23e75d41850ed46a7e2c68
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-08-12 14:56:52 +0000

    Move p8x32 uart to directory

---
+++ bb/os/hardware/drivers/serial/core.py
@@ -0,0 +1,12 @@
+#!/usr/bin/evn python
+
+__copyright__ = "Copyright (c) 2011 Sladeware LLC"
+
+# Import PySerial for simulation purposes.
+# Please, see http://pyserial.sourceforge.net/pyserial_api.html for more details
+import serial
+
+from bb.os import Driver
+
+class Uart(Driver):
+    pass
