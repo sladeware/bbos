754672af07137dd24ecc90fed4f5e8e871c4a67a
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-11-23 17:26:06 +0000

    Remove cores.

--- bb/hardware/cores/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-__copyright__ = "Copyright (c) 2011 Sladeware LLC"
-
-from bb.hardware.cores.propeller_cog import PropellerCog
-
--- bb/hardware/parts/processors/propeller_p8x32a.py
+++ bb/hardware/parts/processors/propeller_p8x32a.py
@@ -10,9 +10,23 @@ The Parallax Propeller P8X32A family of microcontrollers contains eight cogs
 The processor contains 32K RAM and 32K ROM globally that all cogs share using a
 multiplexed system bus. There are 32 IO pins."""
 
-from bb.hardware import Processor
-from bb.hardware.cores import PropellerCog
+from bb.hardware.parts import Processor
 from bb.utils.type_check import verify_list
+
+class PropellerCog(Core):
+    """Parallax Propeller processor's cog core.
+
+    A "cog" is a CPU contained within the Propeller processor. Cogs
+    are designed to run independently and concurrently within the same
+    silicon die. They have their own internal memory, configurable
+    counters, video generators and access to I/O pins as well as the
+    system clock. All the cogs in the processor share access to global
+    resources sych as the main RAM/ROM, synchronization resources and
+    specialized monitoring capabilities to know what the other cogs
+    are doing."""
+
+    def __init__(self, mapping=None):
+        Core.__init__(self, "Propeller Cog", mapping)
 
 class PropellerP8X32A(Processor):
     driver="bb.os.drivers.processors.propeller_p8x32"
