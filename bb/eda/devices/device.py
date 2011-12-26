dfd7897513d7048b513c74095ff043766209d107
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-12-26 09:02:22 +0000

    Working on new device struct.

--- bb/eda/devices/device.py
+++ bb/eda/devices/device.py
@@ -6,97 +6,94 @@ import types
 import networkx
 
 from bb.eda.primitives import *
-from bb.eda.components import *
 
-#_______________________________________________________________________________
-
-class Device(Symbol):
-    """This class represents a physical device. Each device can
-    contain one or more parts that are packaged together (e.g. a
-    74HCT32)."""
+class Device(ElectronicPrimitive):
+    """This class represents a physical device. Device is any type of
+    electrical component. It will have functions and properties unique
+    to its type. Each device can contain one or more parts that are
+    packaged together (e.g. a 74HCT32)."""
 
     # Device register keeps complete list of all devices so the instance
     # of device can not be managed by different device managers.
-    part_register = dict()
+    element_register = dict()
 
     def __init__(self):
-        Symbol.__init__(self)
+        ElectronicPrimitive.__init__(self)
         self.__g = networkx.Graph()
 
-    def set_driver(self, driver):
-        self._driver = driver
+    def register_driver(self, driver):
+        pass
 
-    def get_driver(self):
-        if not self._driver:
-            self._driver = self.__get_default_driver()
-        return self._driver
+    def unregister_driver(self, driver):
+        pass
 
     def __get_default_driver(self):
         return None
 
-    def add_part(self, part):
-        if self.has_part(part):
-            return part
-        if id(part) in self.part_register:
-            raise Exception("The part '%s' already belongs to '%s'" \
-                                % (part, self.part_register[id(part)]))
+    def add_element(self, element):
+        if self.has_part(element):
+            return element
+        if id(element) in self.element_register:
+            raise Exception("The element '%s' already belongs to '%s'" \
+                                % (element, self.element_register[id(element)]))
         # Assume designator
-        if part.designator:
-            if self.find_part(part):
+        if element.designator:
+            if self.find_element(element.designator):
                 pass
         else:
-            if not part.designator_format:
-                raise Exception("Part %s does not have designator format." \
-                                    % part)
-            relatives = self.find_parts(by=self.__class__)
-            part.designator = part.designator_format % len(relatives)
-        self.part_register[id(part)] = self
-        self.__g.add_node(part)
-        return part
+            if not element.designator_format:
+                raise Exception("Element %s does not have designator format." \
+                                    % element)
+            relatives = self.find_elements(self.__class__)
+            element.designator = element.designator_format % len(relatives)
+        self.element_register[id(element)] = self
+        self.__g.add_node(element)
+        return element
 
-    def update_part(self, original, updated):
-        """The `original` part is replacing by a `updated` part, whose
-        designator attribute is set to the designator of the original part."""
+    def update_element(self, original, new):
+        """The `original` element is replacing by a `new`, whose
+        designator attribute is set to the designator of the original
+        element."""
         original_designator = original.designator()
-        self.remove_part(original)
-        target.designator(original_designator)
-        self.add_part(updated)
+        self.remove_element(original)
+        new.designator(original_designator)
+        self.add_element(new)
 
-    def remove_part(self, part):
-        self.__g.remove_node(part)
-        del Component.part_register[id(part)]
+    def remove_element(self, element):
+        self.__g.remove_node(element)
+        del Device.element_register[id(element)]
 
-    def get_parts(self):
-        """Return list of devices that belong to this device manager."""
+    def get_elements(self):
+        """Return list of elements that belongs to this device."""
         return self.__g.nodes()
 
-    def find_part(self, by):
+    def find_element(self, by):
         """If there is more than one child matching the search, the first
-        one is returned. In that case, Part.find_parts() should be used."""
-        parts = self.find_parts(by)
-        return parts.pop(0)
+        one is returned. In that case, Device.find_elements() should be used."""
+        elements = self.find_elements(by)
+        return elements.pop(0)
 
-    def find_parts(self, by):
-        """Returns parts of this component that is identified by by,
+    def find_elements(self, by):
+        """Returns elements of this device that is identified by `by`,
         or None if there is no such object.
 
         The by argument can be represented by a function, string, class or
         number. Omitting the by argument causes all object to be matched."""
-        parts = list()
+        elements = list()
         if type(by) == types.TypeType:
-            for part in self.get_parts():
-                if isinstance(part, by):
-                    parts.append(part)
-        return parts
+            for element in self.get_elements():
+                if isinstance(element, by):
+                    elements.append(element)
+        return elements
 
-    def has_part(self, part):
-        return self.__g.has_node(part)
+    def has_element(self, element):
+        return element in self.__elements
 
-    def connect_parts(self, dest, src=None):
+    def connect_elements(self, dest, src=None):
         if not src:
             src = self
         for part in (src, dest):
-            self.add_component(part)
+            self.add_element(element)
         self.__g.add_edge(src, dest)
 
     def disconnect_parts(self, src, dest):
