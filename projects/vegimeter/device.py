d4a54868351c78a6b23898d845e1156e788008bd
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2011-12-30 15:25:34 +0000

    Some updates.

--- projects/vegimeter/device.py
+++ projects/vegimeter/device.py
@@ -12,13 +12,21 @@ if getpass.getuser() == "d2rk":
 Fritzing.add_search_path(os.path.join(module.get_dir(), "parts"))
 
 vegimeter_device = Fritzing.parse("device.fz")
-# Let us create bill of materials
-bill_of_materials = dict()
-for element in vegimeter_device.get_elements():
-    name = element.get_property_value("name")
-    if name not in bill_of_materials:
-        bill_of_materials[name] = 0
-    bill_of_materials[name] += 1
+#for element in vegimeter_device.get_elements():
+#    print element.get_property_value("name"), element.designator
+
+print vegimeter_device.find_element("R1")
+exit(0)
+
+def bill_of_materials():
+    bill_of_materials = dict()
+    for element in vegimeter_device.get_elements():
+        name = element.get_property_value("name")
+        if name not in bill_of_materials:
+            bill_of_materials[name] = 0
+        bill_of_materials[name] += 1
+    return bill_of_materials
+
 print "Vegimeter bill of materials:"
-for name, amount in bill_of_materials.items():
+for name, amount in bill_of_materials().items():
     print "\t%d %s(s)" % (amount, name)
