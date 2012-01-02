5105b64a0a04349c2ccd93a240fe4e1c479a08f9
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-01-02 15:12:03 +0000

    Few updates.

--- bb/builder/compilers/catalina/lib/catalina_lmmk.c
+++ bb/builder/compilers/catalina/lib/catalina_lmmk.c
@@ -9,22 +9,24 @@
  * Include the dynamic kernel formatted as a C array. The array was produced
  * using the following commands:
  *
- *       homespun Catalina_LMM_dynamic.spin -b -o LMM
- *       spinc LMM.binary > LMM_array.h
+ *       homespun Catalina_LMM_dynamic.spin -b -o catalina_lmm
+ *       spinc catalina_lmm.binary > lmm_array.h
  */
-#include <LMM_array.h>
+#include <lmm_array.h>
 
 /**
- * Start a C function in a new available cog.
+ * Load LMM kernel into selected cog to start specified C function on it.
  *
- * @func: address of C function to run (defined as 'void f(void)')
+ * @function: address of C function to run (defined as 'void f(void)')
  * @stack: address of TOP of stack to use (i.e. points just after last long)
  * @cog_id: cog ID.
  *
  * @return cog used, or -1 on any error
+ *
+ * @seealso: _coginit().
  */
 cog_id_t
-LMMK_init_cog(void function(void), unsigned long* stack, cog_id_t cog_id)
+lmm_init_cog(void function(void), unsigned long* stack, cog_id_t cog_id)
 {
    cog_id_t cog;
    struct {
@@ -36,7 +38,7 @@ LMMK_init_cog(void function(void), unsigned long* stack, cog_id_t cog_id)
    cog_data.REG = _registry(); /* regisrtry address */
    cog_data.PC  = (unsigned long)function; /* address of C function */
    cog_data.SP  = (unsigned long)stack; /* top of stack */
-   cog = _coginit((int)&cog_data >> 2, (int)LMM_array >> 2, cog_id);
+   cog = _coginit((int)&cog_data >> 2, (int)lmm_array >> 2, cog_id);
    delay_ms(100); /* small delay for cog to initialize */
    return cog;
 }
