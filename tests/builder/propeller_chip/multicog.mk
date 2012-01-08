0f0d1b8d06f682e43b8e56945f97f673d2467d0f
Slade Maurer <slade@computer.org>
2012-01-08 02:47:36 +0000

    I just installed Catalina 3.4 32-bit freshly downloaded from sourceforge and the naming uses a lower case d so reflecting that change in the multicog makefile.

--- tests/builder/propeller_chip/multicog.mk
+++ tests/builder/propeller_chip/multicog.mk
@@ -3,7 +3,7 @@ LCC_DIR=/usr/local/lib/catalina
 CATALINA_SUPPORT_DIR = ../../../bb/builder/compilers/catalina
 
 all:
-	homespun -b $(LCC_DIR)/target/Catalina_LMM_Dynamic.spin -o catalina_lmm
+	homespun -b $(LCC_DIR)/target/Catalina_LMM_dynamic.spin -o catalina_lmm
 	spinc catalina_lmm.binary > catalina_lmm_array.h
 	dos2unix LMM_array.h
 	catalina -lci -DDEMO -DPC -I. -I$(CATALINA_SUPPORT_DIR)/include $(CATALINA_SUPPORT_DIR)/lib/catalina_lmm.c $(CATALINA_SUPPORT_DIR)/lib/catalina_time.c multicog.c -o multicog
