cdc7742646447fbca38eaaba324b626452bc2bdd
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-01-08 18:52:23 +0000

    Few simple updates before removing Makefile for multicog test.

--- tests/builder/propeller_chip/multicog.mk
+++ tests/builder/propeller_chip/multicog.mk
@@ -3,7 +3,4 @@ LCC_DIR=/usr/local/lib/catalina
 CATALINA_SUPPORT_DIR = ../../../bb/builder/compilers/catalina
 
 all:
-	homespun -b $(LCC_DIR)/target/Catalina_LMM_dynamic.spin -o catalina_lmm
-	spinc catalina_lmm.binary > catalina_lmm_array.h
-	dos2unix LMM_array.h
-	catalina -lci -DDEMO -DPC -I. -I$(CATALINA_SUPPORT_DIR)/include $(CATALINA_SUPPORT_DIR)/lib/catalina_lmm.c $(CATALINA_SUPPORT_DIR)/lib/catalina_time.c multicog.c -o multicog
+	catalina -lci -lmulticog -DDEMO -DPC -I. multicog.c -o multicog
