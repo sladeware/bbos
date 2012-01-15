453d01724432b2ff015754c7bd24d7eea3f7c3fd
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-01-15 11:03:27 +0000

    Just blink one LED.

--- main.c
+++ main.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
+#include <catalina_cog.h>
 
 void main() {
+  int i = 0;
+ 
 
-  _dira(1 << 16, 1 << 16);
-  _outa(1 << 16, 1 << 16);
-  _dira(1 << 17, 1 << 17);
-  _outa(1 << 17, 1 << 17);
-  _dira(1 << 18, 1 << 18);
-  _outa(1 << 18, 1 << 18);
-  _dira(1 << 19, 1 << 19);
-  _outa(1 << 19, 1 << 10);
-  _dira(1 << 20, 1 << 20);
-  _outa(1 << 20, 1 << 20);
-
-  //delay_ms(1000);
-  //printf("Hello!\n");
-  //delay_ms(1000);
+  _dira(1 << 23, 1 << 23);
+  _outa(1 << 23, 1 << 23);
 
   while(1) {
   }; // loop forever
