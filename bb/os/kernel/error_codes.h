546300d40afdc0e9795a67b5c99ed2ab6d90e6d3
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-01-15 20:07:50 +0000

    Add missing files.

---
+++ bb/os/kernel/error_codes.h
@@ -0,0 +1,15 @@
+/*
+ * Kernel error codes.
+ *
+ * Copyright (c) 2012 Sladeware LLC
+ */
+#ifndef __BBOS_KERNEL_ERROR_CODES_H
+#define __BBOS_KERNEL_ERROR_CODES_H
+
+enum {
+  BBOS_SUCCESS = 0,
+  BBOS_PORT_IS_FULL,
+  BBOS_FAILURE
+};
+
+#endif /* __BBOS_KERNEL_ERROR_CODES_H */
