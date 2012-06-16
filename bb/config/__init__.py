d08d48ac70f812f6fcecbd90fec7abd6ca800033
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-06-16 07:00:29 +0300

    Move config part from the root __init__.

--- bb/config/__init__.py
+++ bb/config/__init__.py
@@ -14,11 +14,18 @@
 
 __copyright__ = "Copyright (c) 2011-2012 Sladeware LLC"
 
-import bb.config
+import os # this is host-os support module, do not mix with bb.os
+import sys
 
-# Do general imports and create common aliases
+# Setup host environment, check compatibility and some dependencies
 
-from bb.app import Mapping, mapping_factory
-from bb.os import OS
+os.environ["BB_HOME"] = \
+  os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 
-BBOS = OS
+# Compatibility with Python 2.5 through 2.7.
+assert (2,5) <= sys.version_info < (3,), """\
+This code is meant for Python 2.5 through 2.7.
+You might find that the parts you care about still work in older
+Pythons or happen to work in newer ones, but you're on your own --
+edit __init__.py if you want to try it."""
+
