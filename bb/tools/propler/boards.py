8cf8a1f82248b917dfed6aba1716a84aa1319e3e
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-06-18 20:03:23 -0700

    Working on uploading process. Add attempts for connection.

--- bb/tools/propler/boards.py
+++ bb/tools/propler/boards.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 __copyright__ = "Copyright (c) 2012 Sladeware LLC"
 __all__ = ["CustomBoardConfig", "QuickStartBoardConfig", "DemoBoardConfig"]
@@ -6,13 +18,24 @@ __all__ = ["CustomBoardConfig", "QuickStartBoardConfig", "DemoBoardConfig"]
 class BoardConfig(object):
     """Base board config."""
 
+    def __init__(self):
+        self.__eeprom_size = getattr(self, "EEPROM_SIZE", None)
+        self.__baudrate = getattr(self, "BAUDRATE", None)
+        self.__chip_version = None
+
+    def set_chip_version(self, version):
+        self.__chip_version = version
+
+    def get_chip_version(self):
+        return self.__chip_version
+
     def get_eeprom_size(self):
         """Return EEPROM size."""
-        return self.EEPROM_SIZE
+        return self.__eeprom_size
 
     def get_baudrate(self):
         """Return baudrate."""
-        return self.BAUDRATE
+        return self.__baudrate
 
 class DemoBoardConfig(BoardConfig):
     """Propeller Demo Board configuration:
