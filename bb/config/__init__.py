c07dff295070cc9a8bb850b3e5c37699c89fe8bb
d2rk <oleks.sviridenko@gmail.com>
2012-07-21 20:41:58 -0700

    Config python importing.

--- bb/config/__init__.py
+++ bb/config/__init__.py
@@ -12,30 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os # this is host-os support module, do not mix with bb.os
+import os as host_os # this is host-os support module, do not mix with bb.os
 import inspect
 import sys
+import platform
 
 import bb.config.py_import # override standard __import__
-
-# Setup host environment, check compatibility and some dependencies
-
-# TODO: choose the right names
-
-os.environ['BB_HOME'] = \
-  os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../.."))
-
-bb_package_file = inspect.getsourcefile(sys.modules['bb'])
-bb_package_dir = os.path.abspath(os.path.join(os.path.dirname(bb_package_file),
-                                              '..'))
-os.environ['BB_PACKAGE_HOME'] = bb_package_dir
-os.environ['BB_APPLICATION_HOME'] = os.path.dirname(os.path.join(sys.path[0],
-                                                                 sys.argv[0]))
-
 
 # Compatibility with Python 2.5 through 2.7.
 assert (2,5) <= sys.version_info < (3,), """\
 This code is meant for Python 2.5 through 2.7.
 You might find that the parts you care about still work in older
 Pythons or happen to work in newer ones, but you're on your own --
-edit __init__.py if you want to try it."""
+edit __init__.py if you want to try it.
+"""
+
+# Setup host environment, check compatibility and some dependencies
+if not getattr(bb, 'env', None):
+    setattr(bb, 'env', dict())
+
+# TODO: choose the right names
+
+bb.env['BB_HOST_OS'] = platform.system() # NOTE: what about os.name?
+bb.env['BB_HOST_PROCESSOR'] = platform.processor()
+bb.env['BB_HOST_ARCH'] = platform.machine()
+bb.env['BB_HOME'] = host_os.path.abspath(
+    host_os.path.join(
+        host_os.path.dirname(
+            host_os.path.realpath(__file__)), "../..")
+    )
+bb_package_file = inspect.getsourcefile(sys.modules['bb'])
+bb_package_dir = host_os.path.abspath(
+    host_os.path.join(host_os.path.dirname(bb_package_file), '..')
+    )
+bb.env['BB_PACKAGE_HOME'] = bb_package_dir
+
+bb.env['BB_APPLICATION_HOME'] = host_os.path.realpath(host_os.curdir)
+sys.path.append(bb.env['BB_APPLICATION_HOME'])
+
+def update_env():
+    for k, v in bb.env.items():
+        host_os.environ[k] = v
