b36988837995c0d7198b5a74f52f5940704811d5
d2rk <oleks.sviridenko@gmail.com>
2012-08-03 12:06:29 -0700

    Fix issue #16. Update projects blinker and helloworld.

--- projects/helloworld/model.py
+++ projects/helloworld/model.py
@@ -5,5 +5,3 @@ from bb.hardware.devices.processors import PropellerP8X32A_Q44
 
 hello_world = bb.Mapping("HELLO_WORLD", processor=PropellerP8X32A_Q44())
 hello_world.register_thread(bb.Thread("PRINTER", "printer_runner"))
-
-bb.application.register_mapping(hello_world)
