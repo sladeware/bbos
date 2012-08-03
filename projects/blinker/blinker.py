d724f2bd2a4722f61cd932a75d750e376a5ddba3
d2rk <oleks.sviridenko@gmail.com>
2012-08-03 00:00:27 -0700

    Start a new demo project that will blinks an LED on the board.

---
+++ projects/blinker/blinker.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import bb
+from bb.hardware.devices.boards import P8X32A_QuickStartBoard
+
+blinking_device = P8X32A_QuickStartBoard()
+# Note, on P8X32A QuickStartBoard schematic the processor is marked as U1.
+blinker = bb.Mapping('Blinker', processor=blinking_device.find_element('U1'))
+blinker.register_thread(bb.Thread('BLINKER', 'blinker_runner'))
+
+bb.application.register_mapping(blinker)
