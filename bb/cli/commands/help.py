33e876450d6d7cc2764f78d980d300ba2b627fee
d2rk <oleks.sviridenko@gmail.com>
2012-08-04 13:10:36 -0700

    Fix issue #14
    Move CLI commands from the single module to the directory, where each command is
    represented by a special module. Now each command is also has to be defined as a
    class.

---
+++ bb/cli/commands/help.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+__copyright__ = 'Copyright (c) 2012 Sladeware'
+
+import sys
+
+from bb.cli import CLI
+from bb.cli.commands.command import Command
+
+class help(Command):
+  USAGE = '%prog help <command>'
+  SHORT_DESC = 'Print help for a specific command'
+  USES_BASEPATH = False
+
+  def function(self, command_name=None):
+    """Prints help for a specific command.
+
+    Args:
+    command: If provided, print help for the action provided.
+
+    Expects self.args[0], or 'command', to contain the name of the action in
+    question. Exits the program after printing the help message.
+    """
+    if not command_name:
+      if len(CLI.config.args) >= 1:
+        CLI.config.args = [' '.join(CLI.config.args)]
+      if len(CLI.config.args) != 1 or \
+            not CLI.is_supported_command(CLI.config.args[0]):
+        CLI.config.optparser.error('Expected a single command argument. '
+                                   ' Must be one of:\n' +
+                                   CLI.get_command_descriptions())
+      command_name = CLI.config.args[0]
+    command = CLI.get_command(command_name)
+    optparser, unused_options = CLI.config._make_specific_parser(command)
+    optparser.print_help()
+    sys.exit(0)
