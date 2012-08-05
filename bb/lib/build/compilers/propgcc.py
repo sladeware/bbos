cbe4048494043171dbaf7f548a037ee8bc5ef21f
d2rk <oleks.sviridenko@gmail.com>
2012-08-05 10:51:04 -0700

    Add more copyright notes. Add new bb env variable BB_BUILD_DIR_NAME.

--- bb/lib/build/compilers/propgcc.py
+++ bb/lib/build/compilers/propgcc.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-__copyright__ = "Copyright (c) 2012 Sladeware LLC"
-__author__ = "Oleksandr Sviridenko"
+__copyright__ = 'Copyright (c) 2012 Sladeware LLC'
+__author__ = 'Oleksandr Sviridenko'
 
 from bb.lib.utils import typecheck
 from bb.lib.build.compilers.unix_c_compiler import UnixCCompiler
@@ -20,7 +32,7 @@ class PropGCCCompiler(UnixCCompiler):
   to learn more about Propeller specific options.
   """
 
-  DEFAULT_EXECUTABLES = {
+  EXECUTABLES = {
     "compiler"     : ["propeller-elf-gcc"],
     "linker_exe"   : ["propeller-elf-gcc"],
     "objcopy"      : ["propeller-elf-objcopy"]
