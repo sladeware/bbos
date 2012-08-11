89533ee6db19089a383e318fd769f52890cb08de
d2rk <oleks.sviridenko@gmail.com>
2012-08-11 14:36:41 -0700

    Fix blinker project. Add two threads for blinking. Move loading to separate file to do not break the beauty of build script for now.

--- projects/blinker/blinker.py
+++ projects/blinker/blinker.py
@@ -6,4 +6,5 @@ from bb.hardware.devices.boards import P8X32A_QuickStartBoard
 blinking_device = P8X32A_QuickStartBoard()
 # Note, on P8X32A QuickStartBoard schematic the processor is marked as U1.
 blinker = bb.Mapping('Blinker', board=blinking_device)
-blinker.register_thread(bb.os.Thread('BLINKER', 'blinker_runner'))
+blinker.register_threads([bb.os.Thread('B0', 'b0_runner'),
+                          bb.os.Thread('B1', 'b1_runner')])
