356d4e982e484d2da877e3bcf98ccb0826042bd8
d2rk <oleks.sviridenko@gmail.com>
2012-08-11 14:39:13 -0700

    Finally add second thread.

---
+++ projects/blinker/b1.c
@@ -0,0 +1,16 @@
+// B1 thread
+
+#include <bb/os.h>
+#include BBOS_PROCESSOR_FILE(pins.h)
+#include BBOS_PROCESSOR_FILE(delay.h)
+
+#define LED_PIN 17
+#define MASK (1 << LED_PIN)
+static int8_t led_state = 0;
+
+void b1_runner() {
+  DIR_TO(LED_PIN, led_state);
+  OUT_TO(LED_PIN, led_state);
+  led_state ^= 1;
+  bbos_delay_sec(1);
+}
