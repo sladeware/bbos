fc148878deb8de686219668ee448ee7ac2028134
d2rk <oleks.sviridenko@gmail.com>
2012-08-11 10:53:32 -0700

    Fix blinker project, since Thread has been moved.

--- projects/blinker/blinker.py
+++ projects/blinker/blinker.py
@@ -6,4 +6,4 @@ from bb.hardware.devices.boards import P8X32A_QuickStartBoard
 blinking_device = P8X32A_QuickStartBoard()
 # Note, on P8X32A QuickStartBoard schematic the processor is marked as U1.
 blinker = bb.Mapping('Blinker', board=blinking_device)
-blinker.register_thread(bb.Thread('BLINKER', 'blinker_runner'))
+blinker.register_thread(bb.os.Thread('BLINKER', 'blinker_runner'))
