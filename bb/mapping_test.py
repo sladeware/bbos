477975b57b5f2ff576543dcb5367453404a8e74c
d2rk <oleks.sviridenko@gmail.com>
2012-08-11 10:24:41 -0700

    Fix tests. Fix indentions.

--- bb/mapping_test.py
+++ bb/mapping_test.py
@@ -17,16 +17,16 @@ import unittest
 import bb
 
 class MappingTest(unittest.TestCase):
-    def testThreadRegistration(self):
-        t1 = bb.Thread("T1")
-        t2 = bb.Thread("T2")
-        mapping = bb.Mapping("M1")
-        mapping.register_threads([t1, t2])
-        self.assertEqual(len(mapping.get_threads()), 2)
+  def testThreadRegistration(self):
+    t1 = bb.os.Thread("T1")
+    t2 = bb.os.Thread("T2")
+    mapping = bb.Mapping("M1")
+    mapping.register_threads([t1, t2])
+    self.assertEqual(len(mapping.get_threads()), 2)
 
-    def testNaming(self):
-        mapping = bb.Mapping("M1")
-        self.assertEqual(mapping.get_name(), "M1")
+  def testNaming(self):
+    mapping = bb.Mapping("M1")
+    self.assertEqual(mapping.get_name(), "M1")
 
 if __name__ == '__main__':
     unittest.main()
