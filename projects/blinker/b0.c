9aaa68ff7dc26f1df2bcee00d08272776b22925d
d2rk <oleks.sviridenko@gmail.com>
2012-08-17 13:01:29 -0700

    Blink LED with ID of running cog.

--- projects/blinker/b0.c
+++ projects/blinker/b0.c
@@ -4,13 +4,11 @@
 #include BBOS_PROCESSOR_FILE(pins.h)
 #include BBOS_PROCESSOR_FILE(delay.h)
 
-#define LED_PIN 16
-#define MASK (1 << LED_PIN)
 static int8_t led_state = 0;
 
 void b0_runner() {
-  DIR_TO(LED_PIN, led_state);
-  OUT_TO(LED_PIN, led_state);
+  DIR_TO(16 + cogid(), led_state);
+  OUT_TO(16 + cogid(), led_state);
   led_state ^= 1;
   bbos_delay_sec(1);
 }
