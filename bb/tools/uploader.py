d536b6371beff0a53b67a9649164d746e537da5f
d2rk <oleks.sviridenko@gmail.com>
2012-08-28 19:58:33 -0700

    Add load support for blinker.

--- bb/tools/uploader.py
+++ bb/tools/uploader.py
@@ -7,13 +7,6 @@ import bb
 from bb.tools import builder
 from bb.tools.loaders import propler
 
-    #for image in bb.Builder.get_application().get_images():
-    #  uploader = propler.SPIUploader(port='/dev/ttyUSB0')
-    #  if not uploader.connect():
-    #    sys.exit(1)
-    #  uploader.upload_file(image.get_name(), eeprom=False)
-    #  uploader.disconnect()
-
 def _import_load_scripts():
   load_script_filename = "load.py"
   load_script_path = bb.host_os.path.join(bb.env['BB_APPLICATION_HOME'],
