1336ad1a40754362dc1d45e32c806a2f6283fd7e
Alexander Sviridenko <oleks.sviridenko@gmail.com>
2012-09-28 21:52:07 +0300

    Update importer.

--- bb/config/compilers/python/importer.py
+++ bb/config/compilers/python/importer.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 __copyright__ = "Copyright (c) 2012 Sladeware LLC"
 __author__ = "Oleksandr Sviridenko"
@@ -13,7 +25,7 @@ import bb
 
 logging.basicConfig(level=logging.DEBUG)
 
-class BBImporter(object):
+class Importer(object):
   """Read more about import hooks here
   <http://www.python.org/dev/peps/pep-0302/>,
   <http://docs.python.org/py3k/library/importlib.html>,
@@ -107,4 +119,4 @@ class BBImporter(object):
       self.include_autogen_module()
     return self._mod
 
-#sys.meta_path = [BBImporter()]
+sys.meta_path = [Importer()]
